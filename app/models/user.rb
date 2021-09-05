1f47cc05e33d0c660ec62b33c42fe2883409696d
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-05 20:22:17 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  with_options presence: true do
+  validates :nickname
+  validates :birth_date
+  validates :gender
+end
 
   enum gender: { man: 0, woman: 1}
 
