30bc8bfb90b107ba0a0d8ece6dc7f9c8c510283a
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-08 16:47:59 +0900

    チャットルーム作成画面の実装

--- db/migrate/20210908013423_create_rooms.rb
+++ db/migrate/20210908013423_create_rooms.rb
@@ -1,7 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.integer :category_id, null: false
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_09_08_021027) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "category_id", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
