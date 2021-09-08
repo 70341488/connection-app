44ad23fdab0a02f944e25537f4def016aa61f6d7
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-08 11:31:14 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  has_many :room_users
+  has_many :rooms, through: :room_users
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
   with_options presence: true do
