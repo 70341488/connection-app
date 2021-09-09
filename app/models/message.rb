4f61de2baf52e4467ef04f75be0215dfe079cbf2
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-09 20:21:15 +0900

    バリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,12 @@ class Message < ApplicationRecord
 
   belongs_to :user
   belongs_to :room
+
+  with_options presence: true do
+    validates :title
+    validates :explanation
+  end
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
