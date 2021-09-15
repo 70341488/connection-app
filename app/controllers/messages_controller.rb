3361be832d4ead235d9713f7ee8bd91b19b8d892
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-15 18:13:47 +0900

    画像以外の非同期完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,6 +11,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
+      ActionCable.server.broadcast 'message_channel', {message: @message, category: @message.category.name, prefectures: @message.prefectures.name}
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
