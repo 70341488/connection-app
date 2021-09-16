618139069efa8ca12ecc15be58e84e7a4166cdf8
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-16 17:37:52 +0900

    preview.jsのリファタクリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('message_image')){
   const ImageList = document.getElementById('image-list');
-  document.getElementById('message_image').addEventListener('change', function(e){
-    
+
+  // 選択した画像を表示する関数
+  const createImageHTML = (blob) => {
+    // 画像を表示するためのdiv要素を生成
+   const imageElement = document.createElement('div');
+
+   // 表示する画像を生成
+   const blobImage = document.createElement('img');
+   blobImage.setAttribute('src', blob);
+
+   // 表示する画像の大きさを調整
+   blobImage.classList.add('preview-size');
+
+   // 生成したHTMLの要素をブラウザに表示させる
+   imageElement.appendChild(blobImage);
+   ImageList.appendChild(imageElement);
+ };
+
+   document.getElementById('message_image').addEventListener('change', function(e){
+
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
-    // 画像を表示するためのdiv要素を生成
-    const imageElement = document.createElement('div');
-    // 表示する画像を生成
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
-
-    // 表示する画像の大きさを調整
-    blobImage.classList.add('preview-size');
-
-    // 生成したHTMLの要素をブラウザに表示させる
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+    createImageHTML(blob);
    });
   }
 });
