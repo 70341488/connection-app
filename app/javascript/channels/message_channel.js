d4c5300b08b896be8a1c5df6c2ebe10a29fc1c13
70341488 <88077407+70341488@users.noreply.github.com>
2021-09-23 18:35:35 +0900

    非同期での画像の表示

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -10,14 +10,16 @@ consumer.subscriptions.create("MessageChannel", {
   },
  
   received(data) {
-    // const html = `<p>${data.nickname}${data.message.created_at}${data.message.title}${data.category}${data.area}${data.message.municipalities}${data.message.image}</p>`;
     const title = `<p>${data.message.title}</p>`;
     const createdAt = `<p>${data.message.created_at}</p>`;
     const nickName = `<p>${data.nickname}</p>`;
     const Category = `<p>${data.category}</p>`;
     const Area = `<p>${data.area}</p>`;
     const Municipalities = `<p>${data.message.municipalities}</p>`;
-    const image = document.getElementById('message-image');
+    // const image = document.images['message-image']
+    // const image = document.getElementById('message-image');
+    const image = data.image;
+    // const image = document.getElementById('js-image');
     console.log(image)
     // debugger
     const html = `
